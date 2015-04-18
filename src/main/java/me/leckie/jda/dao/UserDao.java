31be81dcae02727ab57a311d555b2e72c1d8031b
Leckie <603418904@qq.com>
2015-04-18 18:22:46 +0800

    checkstyle

--- src/main/java/me/leckie/jda/dao/UserDao.java
+++ src/main/java/me/leckie/jda/dao/UserDao.java
@@ -1,18 +1,18 @@
 package me.leckie.jda.dao;
 
-import java.util.List;
-
 import me.leckie.jda.moudle.User;
+
+import java.util.List;
 
 public interface UserDao {
 
-	public long save(User user) throws Exception;
+    public long save(User user) throws Exception;
 
-	public void update(User user) throws Exception;
+    public void update(User user) throws Exception;
 
-	public void delete(long id) throws Exception;
+    public void delete(long id) throws Exception;
 
-	public List< User > list() throws Exception;
+    public List<User> list() throws Exception;
 
-	public User get(long id) throws Exception;
+    public User get(long id) throws Exception;
 }
